1ff57fbc2f87a147bd2ae945eab1225e0b1096e6
oYPIo <voice2of2reason@gmail.com>
2025-06-25 10:32:23 +0300

    feat(LLM): add model service

---
+++ LLM/app/main.py
@@ -0,0 +1,19 @@
+import os
+from fastapi import FastAPI, Request
+from model import Model
+import json
+
+
+model = Model('./model')
+app = FastAPI()
+
+@app.post("/predict")
+async def predict(request: Request):
+    data = await request.body()
+    data_str = data.decode("utf-8")
+    data_dict = json.loads(data_str)
+
+    pred = model.predict(data_dict['text'], n_labels=2)
+    resp = ', '.join([f"{label['label']}: {label['score']}" for label in pred])
+    print('responce:', resp)
+    return {"prediction": resp}
---
+++ LLM/app/model.py
@@ -0,0 +1,42 @@
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
+
+class Model():
+    def __init__(self, model_path):
+        tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+        model = AutoModelForSequenceClassification.from_pretrained(model_path)
+
+        self.label_names = ['sadness', 'joy', 'love', 'anger', 'fear', 'surprise']
+
+        self.pipe = pipeline(
+            "text-classification",
+            model=model,
+            tokenizer=tokenizer,
+            return_all_scores=True,
+        )
+
+    def predict(self, text, n_labels=1):
+        preds = self.pipe(text)[0]
+        preds = sorted(preds, key= lambda x: x['score'], reverse=True)
+
+        for pred in preds:
+            label_id = int(pred["label"].replace("LABEL_", ""))
+            label_name = self.label_names[label_id]
+            pred['label_name'] = label_name
+
+        n_preds = preds[:n_labels]
+
+        return [{'label': pred['label_name'], 'score': f'{pred["score"]:.3f}'} for pred in n_preds]
+
+# def load_model():
+#     return Model("./models/bert-base-uncased/checkpoint-14")
+
+# def examle():
+#     model = Model("./models/bert-base-uncased/checkpoint-14")
+
+#     texts = ['UwU', 'Please, put the gun down. Oh, God, I have kinds.', 'This is so stupid. Why me? ', 'I like the way it burns, the way people run']
+
+#     for text in texts:
+#         print('text:', text)
+#         pred = model.predict(text, n_labels=2)
+#         resp = ', '.join([f"{label['label']}: {label['score']}" for label in pred])
+#         print('responce:', resp)
